8b6c2d511c7c1cc3d526382dee9d560a6a482309
victor.artemov <victor.artemov@mail.ru>
2020-08-28 13:41:14 +0300

    init

---
+++ src/main/java/com/example/demo/VisitsRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VisitsRepository extends CrudRepository<Visit, Long> {
+    Visit findByDescription(String desadas);
+}
